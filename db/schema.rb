20d778105eb2a0df8ed2a5cfdc417e5ae6e063ca
aks-hassaan <hassan.shaukat@activekeysolutions.com>
2021-09-10 12:22:51 +0500

    solved edit issue

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2021_09_07_102940) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,24 +38,16 @@ ActiveRecord::Schema.define(version: 2021_09_07_102940) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.string "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.string "status"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
@@ -67,18 +62,9 @@ ActiveRecord::Schema.define(version: 2021_09_07_102940) do
     t.string "commentable_type"
   end
 
-  create_table "favorite_blogs", force: :cascade do |t|
-    t.integer "blog_id", null: false
-    t.integer "comment_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["blog_id"], name: "index_favorite_blogs_on_blog_id"
-    t.index ["comment_id"], name: "index_favorite_blogs_on_comment_id"
-  end
-
   create_table "favorites", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "blog_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "blog_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["blog_id"], name: "index_favorites_on_blog_id"
@@ -114,8 +100,6 @@ ActiveRecord::Schema.define(version: 2021_09_07_102940) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "blogs", "users"
-  add_foreign_key "favorite_blogs", "blogs"
-  add_foreign_key "favorite_blogs", "comments"
   add_foreign_key "favorites", "blogs"
   add_foreign_key "favorites", "users"
 end
